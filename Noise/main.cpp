f4428f7959890dc3877e509d75ce860d9e7cf1d0
Shuvi22 <36854919+Shuvi22@users.noreply.github.com>
2022-04-09 16:26:21 +0300

    Update main.cpp

--- Noise/main.cpp
+++ Noise/main.cpp
@@ -6,35 +6,39 @@
 const int HEIGHT = 256;
 const int WIDTH = 256;
 int map[HEIGHT][WIDTH];
+char mapChar[HEIGHT][WIDTH];
 int AMH;
 
 void baseGen();
-void terrainGen(int passage);
+void heightGen(int passage);
+void tileConvert();
 void barierGen();
 void saveTerrain();
 void loadTerrain();
 
 
 int main() {
-	baseGen();
-	terrainGen(4);
-	barierGen();
+	srand(time(0));
+	heightGen(6); //from 4 to 6 good result
+	tileConvert();
 	saveTerrain();
 	return 0;
 }
 
 
-void baseGen() {
-	srand(time(0));
+void heightGen(int passage) {
 	for (int i = 0; i < HEIGHT; ++i) {
 		for (int j = 0; j < WIDTH; ++j) {
+			map[i][j] = 0;
+		}
+	}
+
+	for (int i = 3; i < HEIGHT - 3; ++i) {
+		for (int j = 3; j < WIDTH - 3; ++j) {
 			map[i][j] = rand() % 10;
 		}
 	}
-}
 
-
-void terrainGen(int passage) {
 	for (int passageMade = 0; passageMade < passage; ++passageMade) {
 			for (int i = 0; i < HEIGHT; ++i) {
 				for (int j = 0; j < WIDTH; ++j) {
@@ -48,13 +52,23 @@ void terrainGen(int passage) {
 					if (i - 1 >= 0 && j - 1 < WIDTH) { AMH += map[i-1][j+1]; }
 					if (i + 1 < HEIGHT && j - 1 >= 0) { AMH += map[i+1][j-1]; }
 					AMH = AMH / 9;
-
 					if (AMH >= 9) { AMH = 8; }
 					if (AMH < 0) { AMH = 0; }
 					map[i][j] = AMH;
 					AMH = 0;
 				}
 			}
+	}
+}
+
+
+void tileConvert() {
+	for (int i = 0; i < HEIGHT; ++i) {
+		for (int j = 0; j < WIDTH; ++j) {
+			if (map[i][j] >= 0 && map[i][j] < 4) { mapChar[i][j] = '~';}
+			if (map[i][j] > 3 && map[i][j] < 7) { mapChar[i][j] = ' '; }
+			if (map[i][j] > 6) { mapChar[i][j] = '#'; }
+		}
 	}
 }
 
@@ -75,7 +89,7 @@ void saveTerrain() {
 	std::ofstream fout("MapCreator/map.save");
 	for (int i = 0; i < HEIGHT; ++i) {
 		for (int j = 0; j < WIDTH; ++j) {
-			fout << map[i][j];
+			fout << mapChar[i][j];
 		}
 		fout << std::endl;
 		std::cout << "Saving line: " << i << std::endl;
@@ -91,7 +105,7 @@ void loadTerrain() {
 	for(int i = 0; i < HEIGHT; ++i) {
 		for(int j = 0; j < WIDTH; ++j) {
 			fin >> buff;
-			map[i][j] = buff;
+			mapChar[i][j] = buff;
 		}
 		std::cout << "Loading line: " << i << std::endl;
 	}
