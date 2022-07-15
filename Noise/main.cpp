1b37d952862b70b9de2cafc9993c6296b1d0d86d
Shuvi22 <36854919+Shuvi22@users.noreply.github.com>
2022-07-15 15:30:48 +0300

    Update main.cpp

--- Noise/main.cpp
+++ Noise/main.cpp
@@ -1,58 +1,64 @@
 #include <thread>
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include <ctime>
 
-const int HEIGHT = 256;
-const int WIDTH = 256;
+
+const int HEIGHT = 512;
+const int WIDTH = 512;
 int map[HEIGHT][WIDTH];
 char mapChar[HEIGHT][WIDTH];
 int AMH;
 
-void baseGen();
+int min = 0;
+int max = 99;
+
+int numberOfWater = 40;
+int numberOfLand = 49;
+
+void genBase();
 void heightGen(int passage);
 void tileConvert();
-void barierGen();
 void saveTerrain();
 void loadTerrain();
 
 
 int main() {
 	srand(time(0));
-	heightGen(6); //from 4 to 6 good result
+	genBase();
+	heightGen(4); //from 4 to 6 good result
 	tileConvert();
 	saveTerrain();
 	return 0;
 }
 
-
-void heightGen(int passage) {
+void genBase() {
 	for (int i = 0; i < HEIGHT; ++i) {
 		for (int j = 0; j < WIDTH; ++j) {
-			map[i][j] = 0;
+			map[i][j] = min + rand() % (max - min + 1);
+		}
+	}
+/* island generation
+	for (int i = 50; i < HEIGHT - 50; ++i) {
+		for (int j = 50; j < WIDTH - 50; ++j) {
+			map[i][j] = min + rand() % (max - 20 - min + 1);
 		}
 	}
 
-	for (int i = 3; i < HEIGHT - 3; ++i) {
-		for (int j = 3; j < WIDTH - 3; ++j) {
-			map[i][j] = rand() % 10;
+	for (int i = 100; i < HEIGHT - 100; ++i) {
+		for (int j = 100; j < WIDTH - 100; ++j) {
+			map[i][j] = min + rand() % (max - min + 1);
 		}
-	}
+	}*/
+}
 
+void heightGen(int passage) {
 	for (int passageMade = 0; passageMade < passage; ++passageMade) {
 			for (int i = 0; i < HEIGHT; ++i) {
 				for (int j = 0; j < WIDTH; ++j) {
-					AMH += map[i][j] * 2;
-					if (i - 1 >= 0) { AMH += map[i-1][j]; }
-					if (i + 1 < HEIGHT) { AMH += map[i+1][j]; }
-					if (j - 1 >= 0) { AMH += map[i][j-1]; }
-					if (j + 1 < WIDTH) { AMH += map[i][j+1]; }
-					if (i - 1 >= 0 && j - 1 >= 0) { AMH += map[i-1][j-1]; }
-					if (i + 1 < HEIGHT && j + 1 < HEIGHT ) { AMH += map[i+1][j+1]; }
-					if (i - 1 >= 0 && j - 1 < WIDTH) { AMH += map[i-1][j+1]; }
-					if (i + 1 < HEIGHT && j - 1 >= 0) { AMH += map[i+1][j-1]; }
-					AMH = AMH / 9;
-					if (AMH >= 9) { AMH = 8; }
+					AMH = (map[i][j] + map[i-1][j] + map[i+1][j] + map[i][j-1] + map[i][j+1] + map[i-1][j-1] + map[i+1][j+1] + map[i-1][j+1] + map[i+1][j-1]) / 9;
+					if (AMH > max) { AMH = max; }
 					if (AMH < 0) { AMH = 0; }
 					map[i][j] = AMH;
 					AMH = 0;
@@ -61,27 +67,14 @@ void heightGen(int passage) {
 	}
 }
 
-
 void tileConvert() {
 	for (int i = 0; i < HEIGHT; ++i) {
 		for (int j = 0; j < WIDTH; ++j) {
-			if (map[i][j] >= 0 && map[i][j] < 4) { mapChar[i][j] = '~';}
-			if (map[i][j] > 3 && map[i][j] < 7) { mapChar[i][j] = ' '; }
-			if (map[i][j] > 6) { mapChar[i][j] = '#'; }
+			if (map[i][j] < numberOfWater) { mapChar[i][j] = '~';}
+			if (map[i][j] >= numberOfWater && map[i][j] < numberOfLand) { mapChar[i][j] = ' '; }
+			if (map[i][j] >= numberOfLand) { mapChar[i][j] = '#'; }
 		}
 	}
-}
-
-
-void barierGen() {
-		for (int i = 0; i < HEIGHT; ++i) {
-			for (int j = 0; j < WIDTH; ++j) {
-					if (i - 1 < 0) { map[i][j] = 9; }
-					if (i + 1 == HEIGHT) { map[i][j] = 9; }
-					if (j - 1 < 0) { map[i][j] = 9; }
-					if (j + 1 == WIDTH) { map[i][j] = 9; }
-			}
-		}
 }
 
 
